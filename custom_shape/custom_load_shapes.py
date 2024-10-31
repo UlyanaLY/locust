91aac899c7cc35c020a3ee49912bb598ec782579
UlyanaLY <oulyana@gmail.com>
2024-10-31 20:12:04 +0300

    load shape added

--- custom_shape/custom_load_shapes.py
+++ custom_shape/custom_load_shapes.py
@@ -6,9 +6,22 @@ class MyCustomLoadShape(LoadTestShape):
     match cfg.loadshape_type:
         case "baseline": 
             stages = [
-                {"duration": 60, "users": 1, "spawn_rate": 5}
+                {"duration": 5, "users": 1, "spawn_rate": 1}
             ]
-   
+
+        case "fixedload": 
+            stages = [
+                {"duration": 300, "users": 10, "spawn_rate": 2}
+            ]
+            
+        case "stages": 
+            stages = [
+                {"duration": 300, "users": 20, "spawn_rate": 2},
+                {"duration": 600, "users": 40, "spawn_rate": 2},
+                {"duration": 900, "users": 60, "spawn_rate": 2},
+                {"duration": 1200, "users": 80, "spawn_rate": 2},
+                {"duration": 1500, "users": 100, "spawn_rate": 2}
+            ]                  
 
     def tick(self): # стандартная функция локаста, взятая из документации, для работы с кастомными "Лоад-Шейпами"
         run_time = self.get_run_time()
